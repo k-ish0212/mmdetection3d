fbd0b4bdf9d7f8601fb56cc2dfd75615e649f9d5
kokiishimoto <shamozi212@yahoo.co.jp>
2023-04-19 16:15:32 +0900

    add

--- demo/test.py
+++ demo/test.py
@@ -1,7 +1,16 @@
-print("hello world")
+filenames = [f.name for f in os.scandir("../")]print("hello world")
 
 import os
 
 filenames = [f.name for f in os.scandir("../")]
+
+
+print(filenames)
+
+filenames = [f.name for f in os.scandir("../../")]
+
+print(filenames)
+
+filenames = [f.name for f in os.scandir("../../../")]
 
 print(filenames)
